f4ef9fff4ce449c2882f5e54379cecb128eb286b
Jesse Luo <radishchrist@gmail.com>
2012-11-01 01:06:23 +0800

    change some sources' path

---
+++ BrainFuck/compiler/JLRealCompiler.h
@@ -0,0 +1,14 @@
+//
+//  JLRealCompiler.h
+//  BrainFuck
+//
+//  Created by Jesse Luo on 12-10-27.
+//  Copyright (c) 2012年 Jesse Luo. All rights reserved.
+//
+
+#ifndef __BrainFuck__JLRealCompiler__
+#define __BrainFuck__JLRealCompiler__
+
+#include <iostream>
+
+#endif /* defined(__BrainFuck__JLRealCompiler__) */
---
+++ BrainFuck/compiler/JLRealCompiler.mm
@@ -0,0 +1,9 @@
+//
+//  JLRealCompiler.cpp
+//  BrainFuck
+//
+//  Created by Jesse Luo on 12-10-27.
+//  Copyright (c) 2012年 Jesse Luo. All rights reserved.
+//
+
+#include "JLRealCompiler.h"
